b47a5570fa50a5f75125887146c17f8d3f8f0ca0
albertgreinoecker <albert.greinoecker@gmail.com>
2020-04-11 18:43:19 +0200

    Added PersonAusgabe

---
+++ lesson_examples/src/org/htl/lesson_examples/classes_in_classes/PersonAusgabe.java
@@ -0,0 +1,14 @@
+package org.htl.lesson_examples.classes_in_classes;
+
+public class PersonAusgabe {
+
+	public static void main(String[] args) {
+		Person p1 = new Person("Hubert" ,"Maier" , 16, new Wohnort("Dorfstraße", 6162,"Mutters",31));  
+		Person p2 = new Person("Herbert" ,"Hofer" , 16, new Wohnort("Innrain", 6020,"Innsbruck",3));
+		Person p3 = new Person("Franz" ,"Müller" , 16, new Wohnort("Salzburgerstraße", 4020,"Linz",5));
+		
+		p1.ausgabe();
+		p2.ausgabe();
+		p3.ausgabe();
+	}
+}
